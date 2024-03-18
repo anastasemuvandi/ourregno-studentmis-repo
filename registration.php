700a83bc9381bc0cde2b82737bb83d6fba1404ee
TuriImfura <alexanderturiimfura@gmail.com>
2024-03-18 09:16:33 +0200

    yourregno, the form is added successfully

---
+++ registration.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>USER REGISTRATION</h3>
+    <form>
+        <label>Names</label><br>
+        <input type="text" name="names"><br/>
+        <label>Email</label><br/>
+        <input type="email" name="email"><br/>
+        <label>Password</label><br>
+        <input type="password" name="password" /><br/>
+        <submit>Register</submit>
+    </form>
+</body>
+</html>
